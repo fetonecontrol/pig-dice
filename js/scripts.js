b7813b92b983df9d847b382e7bcd2c04fb291cc8
Michael Watts <Wattsjmichael@gmail.com>
2020-06-18 10:46:22 -0700

    Adds Player Two Functionality - Alert win if >= 100 Total Score
    Co-Authored-by Frederick Ernest <Frederick.Ernest@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,27 +17,36 @@ function Player(pname, turnScore, totalScore) {
 let player1 = new Player("player1", 0, 0);
 let player2 = new Player("player2", 0, 0);
 
-Player.prototype.addScore = function(roll) {
+/*Player.prototype.addScore = function(roll) {
   if (roll === 1) {
     this.turnScore = 0
-    $(".player1").hide();
-    $(".player2").show();
     let output = player1.totalScore;
     $("#p1TotalScore").text(output);
   } else {
     let addRoll = this.turnScore += roll;
     return addRoll;
   }
+}*/
+
+Player.prototype.addScore = function(roll) {
+  if (roll === 1) {
+    this.turnScore = 0
+  } else {
+    let addRoll = this.turnScore += roll;
+    return addRoll;
+  }
 }
-
-
+// Button Logic
 
 // Begin Bussiness Logic for "Hold"
 Player.prototype.hold = function() {
   this.totalScore = this.turnScore + this.totalScore;
+  this.turnScore = 0;
   if (this.totalScore >= 100){
-    alert("You win!");
+    alert("You Win!");
+    this.totalScore = 0;
   }
+
   return this.totalScore;
 }
 
@@ -47,8 +56,12 @@ $(document).ready(function() {
     event.preventDefault();
     let roll = playerOneDiceRoll(1, 6);
     player1.addScore(roll);
-    console.log(player1.turnScore);
+    console.log(player1);
     let p1TurnScore = player1.turnScore;
+    if (roll === 1) {
+      $(".player1").hide();
+      $(".player2").show();
+    }
     $("#p1TurnScore").text(p1TurnScore);
     console.log(player1);
 
@@ -58,13 +71,42 @@ $(document).ready(function() {
     event.preventDefault();
     player1.hold();
     console.log(player1);
-    $(".player1").hide();
-    $(".player2").show();
     let p1TotalScore = player1.totalScore;
     $("#p1TotalScore").text(p1TotalScore);
-  //Need to show turn score for player 1
-//need to add functionality for player 2
+    if (player1.totalScore >= 100){
+      player1.totalScore = 0;
+    }
+    $(".player1").hide();
+    $(".player2").show();
+  });
 
+// Player 2 Roll Button
+  $("form#p2Roll").submit(function(event) {
+    event.preventDefault();
+    let roll = playerOneDiceRoll(1, 6);
+    player2.addScore(roll);
+    console.log(player2);
+    let p2TurnScore = player2.turnScore;
+    if (roll === 1) {
+      $(".player2").hide();
+      $(".player1").show();
+    }
+    $("#p2TurnScore").text(p2TurnScore);
+    console.log(player2);
+
+  });
+
+  $("form#p2Hold").submit(function(event) {
+    event.preventDefault();
+    player2.hold();
+    console.log(player2);
+    let p2TotalScore = player2.totalScore;
+    $("#p2TotalScore").text(p2TotalScore);
+    if (player2.totalScore >= 100){
+      player2.totalScore = 0;
+    }
+    $(".player2").hide();
+    $(".player1").show();
   });
 
 });
